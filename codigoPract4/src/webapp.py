483843414fbcb6df77a5ff184408091d95415307
Ximena Becerril <xbecerrilolivares@gmail.com>
2026-09-23 21:27:23 -0600

    lab: agregar aplicacion vulnerable para analisis DAST

--- codigoPract4/src/webapp.py
+++ codigoPract4/src/webapp.py
@@ -34,20 +34,20 @@ def inicio():
 def buscar():
     nombre = request.args.get("nombre", "")
 
-    resultado_html = """
+    resultado_html = f"""
     <html>
         <head>
             <title>Resultado - SecureCampus</title>
         </head>
         <body>
             <h1>Resultado de búsqueda</h1>
-            <p>Estudiante buscado: {{ nombre }}</p>
+            <p>Estudiante buscado: { nombre }</p>
             <a href="/">Regresar</a>
         </body>
     </html>
     """
 
-    return render_template_string(resultado_html, nombre=nombre)
+    return render_template_string(resultado_html)
 
 
 if __name__ == "__main__":
